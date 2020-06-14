cef2ca0f42550b3fdcdf79d91c06f916163b08a5
Michele Bono <bono.michele94@gmail.com>
2020-06-14 17:05:34 +0200

    Build gapps

--- product.mk
+++ product.mk
@@ -2,3 +2,8 @@ VENDOR_EXTRA_PATH := vendor/extra
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+# gapps
+ifneq ($(WITHOUT_GAPPS),true)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+endif
